949772048c248c88f7c28a2f25e8d28e6f4e3809
stefanploom <stefan.ploom@opilane.hariduskeskus.ee>
2025-12-16 10:21:01 +0200

    Update easyGUI.py

--- easyGUI.py
+++ easyGUI.py
@@ -11,15 +11,13 @@ else:
     
 for _ in iter(int, 1):
     liikumine = ["otse", "paremale", "vasakule", "tagasi"]
-    vajutati = buttonbox("Valige kuhu liikuda: ", choices = liikumine)
+    vajutati = buttonbox("Valige kuhu liikuda: ", choices =liikumine)
     if vajutati == "otse":
-        vajutati = buttonbox("Liikusite otse, valige kuhu liikuda:", choices=liikumine)
+        vajutati = buttonbox("Liikusite otse, valige kuhu liikuda:", choices =liikumine)
     elif vajutati == "paremale":
         vajutati = buttonbox("Liikusite paremale, valige kuhu liikuda:", choices=liikumine)
     elif vajutati == "vasakule":
         vajutati = buttonbox("Liikusite vasakule, valige kuhu liikuda:", choices=liikumine)
     elif vajutati == "tagasi":
         vajutati = buttonbox("Liikusite tagasi, valige kuhu liikuda:", choices=liikumine)
-
-    
     
