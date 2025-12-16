9bf1862ec63d8c7436fec7987c7ae26bd8facfc6
stefanploom <stefan.ploom@opilane.hariduskeskus.ee>
2025-12-16 11:20:09 +0200

    Update easyGUI.py

--- easyGUI.py
+++ easyGUI.py
@@ -1,24 +1,54 @@
 from easygui import *
 import random
-tegelane = enterbox("Sisestage oma tegelase nimi:")
-tugevus = integerbox("Sisestage oma tegelase tugevus:", lowerbound = 1, upperbound = 50)
-elupunkt = integerbox("Sisestage oma tegelase elu punktid:", lowerbound = 1, upperbound = 100)
-if tegelane == None or tugevus == None or elupunkt == None or tegelane == "":
-    msgbox("Palun sisestage õigesti!")
-else:
-    msgbox("Tegelase nimi: " + tegelane + " Tugevus: " + str(tugevus) + " Elupunktid: " + str(elupunkt))
-    
-liikumine = ["otse", "paremale", "vasakule", "tagasi"]
-vajutati = buttonbox("Valige kuhu liikuda: ", choices =liikumine)
+
+# 8x8 labürint: 1 = saab minna, 0 = sein
+laburint = [
+    [1,1,0,1,1,1,1,1],
+    [1,1,1,1,0,0,0,1],
+    [0,1,0,0,0,1,0,1],
+    [0,1,0,0,1,1,1,1],
+    [1,1,1,0,1,0,1,0],
+    [1,0,1,0,1,0,0,0],
+    [1,0,0,0,1,1,1,1],
+    [1,1,1,1,1,0,0,0]
+]
+
+# Algpositsioon
+x = 0  # veerg
+y = 0  # rida
+
+liikumine = ["üles", "paremale", "vasakule", "alla"]
+
+#Tegelase asukoht
+def kuva_asukoht():
+    msgbox(f"Oled ruudus ({x+1}, {y+1})  (rida, veerg)")
+
+kuva_asukoht()
+vajutati = buttonbox("Valige kuhu liikuda:", choices=liikumine)
 
 while True:
-    if vajutati == "otse":
-        vajutati = buttonbox("Liikusite otse, valige kuhu liikuda:", choices=liikumine)
+    vana_x, vana_y = x, y
+
+    if vajutati == "üles":
+        y -= 1
+    elif vajutati == "alla":
+        y += 1
     elif vajutati == "paremale":
-        vajutati = buttonbox("Liikusite paremale, valige kuhu liikuda:", choices=liikumine)
+        x += 1
     elif vajutati == "vasakule":
-        vajutati = buttonbox("Liikusite vasakule, valige kuhu liikuda:", choices=liikumine)
-    elif vajutati == "tagasi":
-        vajutati = buttonbox("Liikusite tagasi, valige kuhu liikuda:", choices=liikumine)
-        
-    
+        x -= 1
+    else:
+        break
+    # Kontrollime, et kas oleme väljaspool 8x8 või seina peal
+    if x < 0 or x > 7 or y < 0 or y > 7 or laburint[y][x] == 0:
+        msgbox("Sinna ei saa minna!")
+        x, y = vana_x, vana_y  # taastame vana asukoha
+    else:
+        kuva_asukoht()
+
+    vajutati = buttonbox("Valige kuhu liikuda:", choices=liikumine)
+
+# Võit kui pääsed labürindist   
+if x == 2 and y == 1:
+        print("Põgenesite labürindist!")
+        exit
