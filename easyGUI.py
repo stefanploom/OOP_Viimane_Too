68bde2a012db7d9a3f9d453802395d58be6d4444
stefanploom <stefan.ploom@opilane.hariduskeskus.ee>
2025-12-16 10:58:01 +0200

    Update easyGUI.py

--- easyGUI.py
+++ easyGUI.py
@@ -1,6 +1,5 @@
 from easygui import *
 import random
-## wtf
 tegelane = enterbox("Sisestage oma tegelase nimi:")
 tugevus = integerbox("Sisestage oma tegelase tugevus:", lowerbound = 1, upperbound = 50)
 elupunkt = integerbox("Sisestage oma tegelase elu punktid:", lowerbound = 1, upperbound = 100)
@@ -9,15 +8,17 @@ if tegelane == None or tugevus == None or elupunkt == None or tegelane == "":
 else:
     msgbox("Tegelase nimi: " + tegelane + " Tugevus: " + str(tugevus) + " Elupunktid: " + str(elupunkt))
     
-for _ in iter(int, 1):
-    liikumine = ["otse", "paremale", "vasakule", "tagasi"]
-    vajutati = buttonbox("Valige kuhu liikuda: ", choices =liikumine)
+liikumine = ["otse", "paremale", "vasakule", "tagasi"]
+vajutati = buttonbox("Valige kuhu liikuda: ", choices =liikumine)
+
+while True:
     if vajutati == "otse":
-        vajutati = buttonbox("Liikusite otse, valige kuhu liikuda:", choices =liikumine)
+        vajutati = buttonbox("Liikusite otse, valige kuhu liikuda:", choices=liikumine)
     elif vajutati == "paremale":
         vajutati = buttonbox("Liikusite paremale, valige kuhu liikuda:", choices=liikumine)
     elif vajutati == "vasakule":
         vajutati = buttonbox("Liikusite vasakule, valige kuhu liikuda:", choices=liikumine)
     elif vajutati == "tagasi":
         vajutati = buttonbox("Liikusite tagasi, valige kuhu liikuda:", choices=liikumine)
+        
     
